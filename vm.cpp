3e88e46b56981b509449894289fea53fbf6d7322
Martin Robinette <martinrobinette@posteo.net>
2022-05-22 09:17:56 -0600

    Added Arithmetic Switch cases and Methods

--- vm.cpp
+++ vm.cpp
@@ -4,5 +4,28 @@ vm.cpp
 */
 
 #include <iostream>
+#include "instructions.cpp"
 using namespace std;
 
+//to be renamed and moved into a class
+//just a spot to temporarily hold swich statement
+void temporarySwitchFunction(int op_code, int operand){
+    instructions instructions;//create instructions object
+    switch(op_code) {
+        case 30://ADD
+            instructions.add(operand);
+            break;
+        case 31://Subtract
+            instructions.subtract(operand);
+            break;
+        case 32://Divide
+            instructions.divide(operand);
+            break;
+        case 33://Multiple
+            instructions.multiply(operand);
+            break;
+        default:
+            //Invalid op code
+            //display appropriate error
+    }
+} 
