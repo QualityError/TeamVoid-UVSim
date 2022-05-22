f5056a1188b3ef68e726b799973da32d4ef7519a
D C <DCprivate@github.com>
2022-05-22 16:19:32 -0600

    Finished input loop

--- main.cpp
+++ main.cpp
@@ -7,8 +7,25 @@ main.cpp
 #include <iostream>
 using namespace std;
 
+const int MAX_INPUT = 100;
 
 int main() {
+
+    Memory m;
+    int input;
+
+    for (int i = 0; i < MAX_INPUT; i++) {
+        cout << i << "? ";
+        cin >> input;
+        if (input == -99999) // Exit condition
+            break;
+        if (input < 1000 || input > 9999) { // Not storing in memory if invalid
+            i--;
+            continue;
+        }
+        m.memory_array[i] = input;
+    }
+    m.print_memory();
 
     return 0;
 }
