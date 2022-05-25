1727bfa2b3898ea0e7573c821b9fb345cefb3cf3
Vadim <vpgeleus@gmail.com>
2022-05-25 16:45:14 -0600

    Added switch statements for the I/O operations as well as their fucntions in the instructions.cpp file. Changed the order of the functions in the vm.cpp file for the temporarySwitchFunction to be seen.

--- vm.cpp
+++ vm.cpp
@@ -8,6 +8,41 @@ vm.cpp
 using namespace std;
 
 
+//to be renamed and moved into a class
+//just a spot to temporarily hold swich statement
+void temporarySwitchFunction(int op_code, int operand){
+    instructions instructions;//create instructions object
+    switch(op_code) {
+      case 10://READ
+        instructions.read(operand, m);
+        break;
+      case 11://WRITE
+        instructions.write(operand, m);
+        break;
+      case 20://LOAD
+          instructions.load(operand, m); // expecting memory object to pass to instructions.cpp
+          break;
+      case 21://STORE
+          instructions.store(operand, m); // expecting memory object to pass to instructions.cpp
+          break;
+      case 30://ADD
+          instructions.add(operand);
+          break;
+      case 31://Subtract
+          instructions.subtract(operand);
+          break;
+      case 32://Divide
+          instructions.divide(operand);
+          break;
+      case 33://Multiple
+          instructions.multiply(operand);
+          break;
+      default:
+          //Invalid op code
+          //display appropriate error
+    }
+}
+
 void VM(Memory m) {
     m.IC = 0;
     int op_code;
@@ -19,34 +54,5 @@ void VM(Memory m) {
         temporarySwitchFunction(op_code, operand); // This function will need to pass the memory object to instructions.cpp
         
         // Increment IC
-    }
-}
-
-//to be renamed and moved into a class
-//just a spot to temporarily hold swich statement
-void temporarySwitchFunction(int op_code, int operand){
-    instructions instructions;//create instructions object
-    switch(op_code) {
-        case 20://LOAD
-            instructions.load(operand, m); // expecting memory object to pass to instructions.cpp
-            break;
-        case 21://STORE
-            instructions.store(operand, m); // expecting memory object to pass to instructions.cpp
-            break;
-        case 30://ADD
-            instructions.add(operand);
-            break;
-        case 31://Subtract
-            instructions.subtract(operand);
-            break;
-        case 32://Divide
-            instructions.divide(operand);
-            break;
-        case 33://Multiple
-            instructions.multiply(operand);
-            break;
-        default:
-            //Invalid op code
-            //display appropriate error
     }
 }
