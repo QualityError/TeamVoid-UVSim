67caf3a1d78a7b4c312fb1ae217b00ab12ade9aa
CJ <cjpryor1416@gmail.com>
2022-05-26 21:25:19 -0600

    Update vm.cpp
    Switch statements for branching. Also changed the case numbers to fall in order with the math case numbers.

--- vm.cpp
+++ vm.cpp
@@ -8,6 +8,54 @@ vm.cpp
 using namespace std;
 
 
+//to be renamed and moved into a class
+//just a spot to temporarily hold swich statement
+void temporarySwitchFunction(int op_code, int operand, Memory m){
+    instructions instructions;//create instructions object
+    switch(op_code) {
+      case 10://READ
+        instructions.read(operand, m);
+        break;
+      case 11://WRITE
+        instructions.write(operand, m);
+        break;
+      case 20://LOAD
+          instructions.load(operand, m); // expecting memory object to pass to instructions.cpp
+          break;
+      case 21://STORE
+          instructions.store(operand, m); // expecting memory object to pass to instructions.cpp
+          break;
+      case 30://ADD
+          instructions.add(operand, m);
+          break;
+      case 31://Subtract
+          instructions.subtract(operand, m);
+          break;
+      case 32://Divide
+          instructions.divide(operand, m);
+          break;
+      case 33://Multiple
+          instructions.multiply(operand, m);
+          break;
+      case 34://Branch
+          instructions.branch(operand, m);
+          break;
+      case 35://BranchNeg
+          instructions.branchneg(operand, m);
+          break;
+      case 36://BranchZero
+          instructions.branchzero(operand, m);
+          break;
+      case 37://HALT
+          instructions.halt(operand, m);
+          break;
+      default:
+          //Invalid op code
+          //display appropriate error
+          break;
+    }
+}
+
 void VM(Memory m) {
     m.IC = 0;
     int op_code;
@@ -19,52 +67,5 @@ void VM(Memory m) {
         temporarySwitchFunction(op_code, operand); // This function will need to pass the memory object to instructions.cpp
         
         // Increment IC
-    }
-}
-
-//to be renamed and moved into a class
-//just a spot to temporarily hold swich statement
-void temporarySwitchFunction(int op_code, int operand){
-    instructions instructions;//create instructions object
-    switch(op_code) {
-        case 30://ADD
-            instructions.add(operand);
-            break;
-        case 31://Subtract
-            instructions.subtract(operand);
-            break;
-        case 32://Divide
-            instructions.divide(operand);
-            break;
-        case 33://Multiple
-            instructions.multiply(operand);
-            break;
-        default:
-            //Invalid op code
-            //display appropriate error
-    }
-} 
-
-//Switch statements for control operations
-void switchStatementsControlOpsFunction(int op_code, int operand) {
-    instructions instructions;
-    switch(op_code) {
-        case 40://Branch
-            instructions.branch(operand);
-            break;
-        case 41://BranchNeg
-            instructions.branchneg(operand);
-            break;
-        case 42://BranchZero
-            instructions.branchzero(operand);
-            break;
-        case 43://HALT
-            instructions.halt(operand);
-            break;
-        default:
-            //Invalid op code
-            //display appropriate error
-
-
     }
 }
