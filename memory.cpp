6f204cd32448500fb51e4f770da03ecea181e8db
Vadim <vpgeleus@gmail.com>
2022-05-28 14:42:37 -0600

    Created a memory visualization function at 'print_memory()'. Removed commented line for memory display in main.cpp

--- memory.cpp
+++ memory.cpp
@@ -4,6 +4,7 @@ memory.cpp
 */
 #include "memory.h"
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 void Memory::set_value(int mem_location, int value) {
@@ -19,7 +20,12 @@ int Memory::get_value(int mem_location) {
 }
 
 void Memory::print_memory() {
-    for (int i = 0; i < 100; i++) {
-        cout << memory_array[i] << " ";
-    }
-}
+    cout<<"MEMORY"<<endl; cout<<"\t00\t\t01\t\t02\t\t03\t\t04\t\t05\t\t06\t\t07\t\t08\t\t09"<<endl; // Column count
+    for(int i = 0; i < 100; i += 10){
+      cout <<"0"<<i*0.1<< "\t"; // Row count
+      for(int j = i; j < 10+i && j < 100; j++){ 
+        cout << setw(5) << setfill('0') <<memory_array[j] << "\t"; // Converting output to string to display a 00000 format of the memory slot
+        }  
+        cout << endl;
+        }
+  }
