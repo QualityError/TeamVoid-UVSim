8df4f5cd88d938694c193fea751cc5281833abad
Vadim Pidoshva <84491549+pidoshva@users.noreply.github.com>
2022-06-07 14:59:14 -0600

    Demo Ready (#61)
    * fixed IC (-99999 isnt counted anymore)
    
    * added required messages prior to execution
    
    * Update main.cpp
    
    * Update main.cpp
    
    changed position of a message
    
    * Update main.cpp

--- main.cpp
+++ main.cpp
@@ -42,17 +42,17 @@ int main() {
         }
         if (input < 1000 || input > 9999) { // Not storing in memory if out of range
             i--;
-            cout << "ML command is the incorrect length, please input a two digit opcode and two digit memory location";
+            cout << "ML command is the incorrect length, please input a two digit opcode and two digit memory location.\n";
             cin.clear();
             cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
             continue;
         }
         m.set_value(i, input);
     }
-
+    
+    cout<<"\n*** Program loading complete ***"<<endl;
+    cout<<"*** Program execution begins ***"<<endl;
     VM(m);
-
-    //m.print_memory(); // visualize memory
     m.dumpMemory();
 
     return 0;
