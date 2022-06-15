0d301d6a51a80cbc19c66e979ce81722c3ae9d7b
Martin Robinette <martinrobinette@posteo.net>
2022-06-15 17:30:57 -0600

    python running UVSim

---
+++ TestProgram.py
@@ -0,0 +1,22 @@
+import subprocess
+
+def main():
+    result = testExample()
+    print("TestExample :", result)
+
+#example inputs ="1000\n1100\n4300\n-99999\n1200\n"
+def runUVSim(inputs):
+    command = "./UVSim.app"
+    #run
+    p = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, shell=True, universal_newlines=True)  
+    output, err = p.communicate(input=inputs)
+    #return
+    return output[output.rfind(" "):-1]  # -1 to strip the final newline
+
+def testExample():
+    inputs ="1000\n1100\n4300\n-99999\n1200\n"
+    output = runUVSim(inputs)
+    #print(output)
+    return True #return true is passes false if failed
+
+main()
